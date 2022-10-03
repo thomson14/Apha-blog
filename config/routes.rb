1e63ce0fb4e595f4c64eeae91eca73db36c43fe8
prashant <prashant.chaurasia@zehntech.com>
2022-10-03 19:00:44 +0530

    add create and destroy and applica helper

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :articles 
   get 'signup' , to: 'users#new'
   resources :users, except: [:new]
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'login', to: 'sessions#destroy'
 end
 
 
