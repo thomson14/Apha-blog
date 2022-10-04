b6c5d416ca4174d063de32c9b339885333bbf5e9
prashant <prashant.chaurasia@zehntech.com>
2022-10-04 10:23:28 +0530

    ristrict action at controll level artciles and users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
     before_action :set_user, only: [:show, :edit, :update]
+    before_action :require_user,only: [:edit, :update]
+    before_action :require_same_user, only:[:edit, :update]
     
     def show
        
@@ -49,5 +51,12 @@ class UsersController < ApplicationController
 
     def set_user
         @user = User.find(params[:id])
+    end
+
+    def require_same_user
+        if current_user != @user
+            flash[:alert] = "you can only edit your account"
+            redirect_to @user
+        end
     end
 end
