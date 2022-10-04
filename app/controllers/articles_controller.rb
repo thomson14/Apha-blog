9fe4750c26d5ff788a08ddba31db8216ba0f77e6
prashant <prashant.chaurasia@zehntech.com>
2022-10-04 12:18:34 +0530

    add amine to the table

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -55,7 +55,7 @@ class ArticlesController < ApplicationController
       end
 
       def require_same_user
-        if current_user != @article.user
+        if current_user != @article.user && !current_user.admin?
           flash[:alert] = "you can only edit or delete your article"
           redirect_to @article 
         end
